042763ab971295a37820461577fa2ba790e50ec9
Legrain Samuel <samuel.legrain@bstorm.be>
2021-09-03 17:29:50 +0200

    Add Exos-01 & Exos-02

---
+++ Exos-02/scripts/main.js
@@ -0,0 +1,13 @@
+let lastname = "Legrain";
+let firstname = 'Samuel';
+let yearsOld = 34 ;
+let gender = false;
+
+let person = {
+    name : lastname,
+    firstname : firstname,
+    yearsOld : yearsOld,
+    isFemale : gender
+};
+
+console.log(person);
