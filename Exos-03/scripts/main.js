f903b3a44cda485011c483bde9edf8053f332038
Legrain Samuel <samuel.legrain@bstorm.be>
2021-09-07 09:38:35 +0200

    Add Exos-03 and Exos-04

---
+++ Exos-03/scripts/main.js
@@ -0,0 +1,20 @@
+//Solution 1
+
+let reponse = prompt('Indiquez votre année de naissance : ');
+let age = 2021 - parseInt(reponse);
+alert('Vous avez '+ age +' ans.');
+
+// //Solution 2
+
+// let reponse = parseInt(prompt('Indiquez votre année de naissance : '));
+// let age = 2021 - reponse;
+// alert('Vous avez '+ age +' ans.');
+
+// //Solution 3
+
+// let reponse = parseInt(prompt('Indiquez votre année de naissance : '));
+// alert(`Vous avez ${2021 - reponse} ans.`);
+
+// //Solution 4
+
+// alert('Vous avez '+ (2021 - parseInt(prompt('Indiquez votre année de naissance : '))) +' ans.');
---
+++ Exos-04/scripts/main.js
@@ -0,0 +1,17 @@
+let phrase = 'ma formation javascript';
+console.log(phrase);
+
+console.log(phrase.indexOf('ma'));
+// console.log(phrase.lastIndexOf('ma'));
+
+console.log(phrase.indexOf('p'));
+
+console.log(phrase.charAt(21));
+// console.log(phrase.substr(21,1));
+// console.log(phrase[21]);
+
+console.log(phrase.replace('javascript', 'Java'));
+
+console.log(phrase.split(" "));
+
+console.log(phrase.split("").reverse().join(''));
